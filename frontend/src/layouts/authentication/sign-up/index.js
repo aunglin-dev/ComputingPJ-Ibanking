5fa86b98effe0f197cc54d5376e86750c149d7eb
Aung_Lin <linoscar724@gmail.com>
2025-03-14 12:06:07 +0630

    [Completed] User Sign Up UI

--- frontend/src/layouts/authentication/sign-up/index.js
+++ frontend/src/layouts/authentication/sign-up/index.js
@@ -1,30 +1,17 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// react-router-dom components
 import { Link } from "react-router-dom";
 
 // @mui material components
 import Card from "@mui/material/Card";
 import Checkbox from "@mui/material/Checkbox";
-
+import { Grid, Chip } from "@mui/material";
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 import MDInput from "components/MDInput";
 import MDButton from "components/MDButton";
+import { useState } from "react";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
 
 // Authentication layout components
 import CoverLayout from "layouts/authentication/components/CoverLayout";
@@ -33,6 +20,11 @@ import CoverLayout from "layouts/authentication/components/CoverLayout";
 import bgImage from "assets/images/bg-sign-up-cover.jpeg";
 
 function Cover() {
+  const [date, setDate] = useState("0001-01-01");
+  const [selectedValues, setSelectedValues] = useState([]);
+  const options = ["Savings Account", "Checking Account", "Business Account", "Student Account"];
+  const Gender = ["Male", "Female"];
+
   return (
     <CoverLayout image={bgImage}>
       <Card>
@@ -51,20 +43,133 @@ function Cover() {
             Join us today
           </MDTypography>
           <MDTypography display="block" variant="button" color="white" my={1}>
-            Enter your email and password to register
+            Enter below information to request an account
           </MDTypography>
         </MDBox>
         <MDBox pt={4} pb={3} px={3}>
           <MDBox component="form" role="form">
             <MDBox mb={2}>
-              <MDInput type="text" label="Name" variant="standard" fullWidth />
+              <Autocomplete
+                multiple
+                options={options}
+                value={selectedValues}
+                onChange={(event, newValue) => {
+                  setSelectedValues(newValue);
+                }}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Type Of Account"
+                    variant="standard"
+                    fullWidth
+                    placeholder="Select or enter account types"
+                  />
+                )}
+                renderTags={(value, getTagProps) =>
+                  value.map((option, key) => (
+                    <Chip
+                      key={key} // Add the key prop here
+                      label={option}
+                      {...getTagProps({ key })} // Pass index to getTagProps
+                      color="primary" // Customize chip color
+                      size="small" // Customize chip size
+                    />
+                  ))
+                }
+                freeSolo
+              />
+            </MDBox>
+            <MDBox mb={2}>
+              <MDInput type="text" label="UserName" variant="standard" fullWidth />
             </MDBox>
             <MDBox mb={2}>
               <MDInput type="email" label="Email" variant="standard" fullWidth />
             </MDBox>
             <MDBox mb={2}>
-              <MDInput type="password" label="Password" variant="standard" fullWidth />
+              <MDInput type="text" label="FullName" variant="standard" fullWidth />
             </MDBox>
+            <MDBox mb={2}>
+              {/* <MDInput type="text" label="NRC" variant="standard" fullWidth /> */}
+
+              <Grid container spacing={2} alignItems="center">
+                {/* Region Input */}
+                <Grid item xs={12} sm={4}>
+                  <MDInput
+                    type="text"
+                    label="Region"
+                    variant="standard"
+                    fullWidth
+                    // value={region}
+                    // onChange={(e) => setRegion(e.target.value)}
+                    // onBlur={handleBlur}
+                    placeholder="12"
+                  />
+                </Grid>
+
+                {/* Township Input */}
+                <Grid item xs={12} sm={4}>
+                  <MDInput
+                    type="text"
+                    label="Township"
+                    variant="standard"
+                    fullWidth
+                    // value={township}
+                    // onChange={(e) => setTownship(e.target.value)}
+                    // onBlur={handleBlur}
+                    placeholder="KAMAYA"
+                  />
+                </Grid>
+
+                {/* Unique Identifier Input */}
+                <Grid item xs={12} sm={4}>
+                  <MDInput
+                    type="text"
+                    label="Unique Identifier"
+                    variant="standard"
+                    fullWidth
+                    // value={uniqueId}
+                    // onChange={(e) => setUniqueId(e.target.value)}
+                    // onBlur={handleBlur}
+                    placeholder="239833"
+                  />
+                </Grid>
+              </Grid>
+            </MDBox>
+            <MDBox mb={2}>
+              {/* <MDInput type="date" label="Date Of Birth" variant="standard" fullWidth /> */}
+
+              <MDInput
+                type="date"
+                label="Date Of Birth"
+                variant="standard"
+                fullWidth
+                value={date}
+                sx={{
+                  "&::-webkit-datetime-edit": {
+                    visibility: "hidden", // Hide the default placeholder
+                  },
+                }}
+              />
+            </MDBox>
+            <MDBox mb={2}>
+              <MDInput type="text" label="Company" variant="standard" fullWidth />
+            </MDBox>
+            <MDBox mb={2}>
+              <MDInput type="text" label="PhoneNumber" variant="standard" fullWidth />
+            </MDBox>
+            <MDBox mb={2}>
+              <MDInput type="text" label="Address" variant="standard" fullWidth />
+            </MDBox>
+            <MDBox mb={2}>
+              <Autocomplete
+                options={Gender}
+                renderInput={(params) => (
+                  <TextField {...params} label="Gender" variant="standard" fullWidth />
+                )}
+                freeSolo
+              />
+            </MDBox>
+
             <MDBox display="flex" alignItems="center" ml={-1}>
               <Checkbox />
               <MDTypography
@@ -88,7 +193,7 @@ function Cover() {
             </MDBox>
             <MDBox mt={4} mb={1}>
               <MDButton variant="gradient" color="info" fullWidth>
-                sign in
+                Request Account
               </MDButton>
             </MDBox>
             <MDBox mt={3} mb={1} textAlign="center">
